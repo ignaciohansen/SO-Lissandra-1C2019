6a21834adde0a256b26991cdd58897d202399fad
Federico A. Enrrique <fede.enrrique@gmail.com>
2019-04-28 10:47:14 -0300

    compila LFS

--- LissandraFileSystem/LissandraFileSystem.c
+++ LissandraFileSystem/LissandraFileSystem.c
@@ -293,3 +293,28 @@ void menu(){
 
 }
 
+// LOGGEA todo lo que escucha.
+int listenSomeLQL(t_log* logger) {
+
+	while(1) {
+
+	}
+
+	imprimirMensaje(logger, " \n ====== LFS Listener: waiting for client connections ====== \n ");
+
+	conexionEntrante = aceptarConexionSocket(socketEscuchaMemoria,logger);
+
+	Puntero buffer = (void*)string_new(); // malloc(sizeof(char)*100);
+
+	recibiendoMensaje = socketRecibir(conexionEntrante, buffer, 25,  logger);
+
+	// buffer[25] = '\0';
+
+	char* msg = string_new();
+	string_append(&msg,"Mensaje recibido: \""); string_append(&msg,buffer ); string_append(&msg,"\"." );
+
+	imprimirVerde(logger, msg);
+
+	free(buffer);
+
+}
