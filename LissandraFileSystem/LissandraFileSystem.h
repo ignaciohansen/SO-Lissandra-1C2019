964705b9817a6c86854aa82a7ad8d6dd3e0724a8
Lorenzo Gutierrez <gutilorenzo@est.frba.utn.edu.ar>
2019-04-29 23:39:46 -0300

    Implementamos logica de comando select, faltando solamente recorrer la particion en busca de key

--- LissandraFileSystem/LissandraFileSystem.h
+++ LissandraFileSystem/LissandraFileSystem.h
@@ -3,23 +3,33 @@
 
 #include "../Biblioteca/src/Biblioteca.c"
 
-#define PATH_LFILESYSTEM_CONFIG "../LFS_CONFIG.txt"
+#define PATH_LFILESYSTEM_CONFIG "LFS_CONFIG.txt"
 #define LOG_PATH "LOG_LFS.txt"
 
 t_log* logger;
 
-typedef struct{
-	char* ip;
+typedef struct{	
 	int puerto_escucha;
 	char* punto_montaje;
 	int retardo;
 	int tamanio_value;
 	int tiempo_dump;
-
-
 }t_lfilesystem_config;
 
+typedef struct{
+	char* consistency;
+	int particiones;
+	int compaction_time;
+
+}t_metadata_tabla;
+
+
+
 t_lfilesystem_config* configFile;
+
+t_metadata_tabla* metadata;
+
+
 char** buffer;
 
 int socketEscuchaMemoria, conexionEntrante, recibiendoMensaje,tamanio;
@@ -40,8 +50,18 @@ void iniciaabrirServidorLissandra();
 #define MAXSIZE_COMANDO 200
 //enum {Select, insert, create, describe, drop, salir};
 char* linea;
+char* tabla_Path;
 void consola();
 void menu();
+
+char *separador2 = "\n";
+char *separator = " ";
+//Comandos
+int comandoSelect(char* tabla, int key);
+
+void obtenerMetadata();
+
+int verificarTabla(char* tabla);
 
 /*--------------------------------------------------------------------------------------------
  * 									Elementos de escucha
