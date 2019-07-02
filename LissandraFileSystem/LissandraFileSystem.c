3f7058a70883523e7405aa4be4037435bd8cb02c
ignaciohansen <ignaciohansen@hotmail.com>
2019-07-01 21:27:56 -0300

    Drop borra .tmpc y avance de Create

--- LissandraFileSystem/LissandraFileSystem.c
+++ LissandraFileSystem/LissandraFileSystem.c
@@ -986,7 +986,6 @@ void esperarTiempoDump() {
 			log_info(logger, "La memtable esta vacia");
 		}
 
-
 	}
 
 }
@@ -1061,13 +1060,13 @@ void crearArchivoTemporal(char* path, char* tabla) {
 		t_registroMemtable* registro;
 		int offset = 0;
 
-		for(int i = 0; i< cantidad_registros; i++){
+		for (int i = 0; i < cantidad_registros; i++) {
 			registro = list_get(listaRegistrosTabla, i);
 
 			tam_total_registros += registro->tam_registro;
 		}
 
-		tam_total_registros += sizeof(char)*3*cantidad_registros;
+		tam_total_registros += sizeof(char) * 3 * cantidad_registros;
 
 		void *registrosAInsertar = malloc(tam_total_registros);
 		registrosAInsertar = string_new();
@@ -1083,13 +1082,15 @@ void crearArchivoTemporal(char* path, char* tabla) {
 			offset += sizeof(u_int16_t);
 			memcpy(registrosAInsertar, &punto_y_coma, sizeof(char));
 			offset += sizeof(char);
-			memcpy(registrosAInsertar, &registro->value, strlen(registro->value));
+			memcpy(registrosAInsertar, &registro->value,
+					strlen(registro->value));
 			offset += strlen(registro->value);
 			memcpy(registrosAInsertar, &barra_n, sizeof(char));
 			offset += sizeof(char);
 
 		}
-		log_info(logger, "Tamanio total de los registros de la %s es: %d",tabla,tam_total_registros);
+		log_info(logger, "Tamanio total de los registros de la %s es: %d",
+				tabla, tam_total_registros);
 
 		//escribirBloque(path, registrosAInsertar, tam_total_registros);
 
@@ -1100,18 +1101,18 @@ void crearArchivoTemporal(char* path, char* tabla) {
 
 		//fwrite((void*)registroAInsertar, 1, strlen(registroAInsertar), temporal);
 		/*
-		log_info(logger, "Datos a insertar en el tmp: \n%s",registrosAInsertar);
-		void* hola = malloc(sizeof(int));
-		int pruebaHola = 3;
-		memcpy(hola, &pruebaHola, sizeof(int));
-		fwrite(&hola, 1, sizeof(int), temporal);*/
+		 log_info(logger, "Datos a insertar en el tmp: \n%s",registrosAInsertar);
+		 void* hola = malloc(sizeof(int));
+		 int pruebaHola = 3;
+		 memcpy(hola, &pruebaHola, sizeof(int));
+		 fwrite(&hola, 1, sizeof(int), temporal);*/
 		/*int int1 = 3;
-		int int2 = 4;
-		void *buffer;
-		buffer = malloc (sizeof(int)); //+ sizeof(int));
-		memcpy (buffer, &int1, sizeof(int));
-		//memcpy (buffer + sizeof(int), &int2, sizeof(int));
-		log_info(logger, "en el buffer: %d", *(int*)buffer);*/
+		 int int2 = 4;
+		 void *buffer;
+		 buffer = malloc (sizeof(int)); //+ sizeof(int));
+		 memcpy (buffer, &int1, sizeof(int));
+		 //memcpy (buffer + sizeof(int), &int2, sizeof(int));
+		 log_info(logger, "en el buffer: %d", *(int*)buffer);*/
 	} else {
 		log_error(logger,
 				"No se pudo realizar el dump pq no hay lugar en el bitmap");
@@ -1124,24 +1125,23 @@ void crearArchivoTemporal(char* path, char* tabla) {
 
 /*void escribirBloque(char* path, void* lista_registros, int tam_registros){
 
-	FILE* archivo;
-	archivo = fopen(path, "wb");
+ FILE* archivo;
+ archivo = fopen(path, "wb");
 
-	fwrite(lista_registros, 1, tam_registros, archivo);
+ fwrite(lista_registros, 1, tam_registros, archivo);
 
-	fclose(archivo);
+ fclose(archivo);
 
-}*/
+ }*/
 
 /*void* leerBloque(char* path){
-	FILE* bloque;
-	bloque = fopen(path, "rb");
+ FILE* bloque;
+ bloque = fopen(path, "rb");
 
-	fread
-}*/
+ fread
+ }*/
 
 //Lectura y escritara de bloques
-
 int determinarParticion(int key, int particiones) {
 
 	log_info(logger, "KEY: %d ", key);
@@ -1158,7 +1158,6 @@ void rutaParticion(int particion) {
 	char * stringParticion = malloc(sizeof(char) * 3);
 
 	sprintf(stringParticion, "%d", particion);
-	log_info(logger, "resultado de sprintf %s", stringParticion);
 
 	archivoParticion = malloc(
 			string_length(tablaAverificar) + string_length(stringParticion)
@@ -1312,36 +1311,53 @@ char* buscarBloque(char* key) {
 
 void eliminarTablaCompleta(char* tabla) {
 
-	obtenerMetadataTabla(tabla);
+	if (obtenerMetadataTabla(tabla) == 0) {
 
-	for (int i = 0; i < metadata->particiones; i++) {
+		for (int i = 0; i < metadata->particiones; i++) {
 
-		rutaParticion(i);
+			rutaParticion(i);
 
-		log_info(logger, "Vamos a eliminar el archivo binario  de la tabla: %s",
-				archivoParticion);
+			log_info(logger,
+					"Vamos a eliminar el archivo binario  de la tabla: %s",
+					archivoParticion);
 
-		int retParticion = remove(archivoParticion);
+			int retParticion = remove(archivoParticion);
 
-		if (retParticion == 0) { // Eliminamos el archivo
-			log_info(logger, "El archivo fue eliminado satisfactoriamente\n");
-		} else {
-			log_info(logger, "No se pudo eliminar el archivo\n");
+			if (retParticion == 0) { // Eliminamos el archivo
+				log_info(logger,
+						"El archivo fue eliminado satisfactoriamente\n");
+			} else {
+				log_info(logger, "No se pudo eliminar el archivo\n");
 
+			}
 		}
+
 	}
 
 	for (int j = 0; j <= cantidad_de_dumps; j++) {
 
 		char* archivoTemporal = armarPathTablaParaDump(tabla, j);
+		char* archivoTemporalC = armarPathTablaParaDump(tabla, j);
+		string_append(&archivoTemporalC, "c");
 
 		log_info(logger,
 				"Vamos a eliminar el archivo temporal  de la tabla: %s",
 				archivoTemporal);
 
+		log_info(logger,
+				"Vamos a eliminar el archivo temporal a compactar de la tabla: %s",
+				archivoTemporalC);
+
 		int retTemporal = remove(archivoTemporal);
+		int retTemporalC = remove(archivoTemporalC);
 
 		if (retTemporal == 0) { // Eliminamos el archivo
+			log_info(logger, "El archivo fue eliminado satisfactoriamente\n");
+		} else {
+			log_info(logger, "No se pudo eliminar el archivo\n");
+		}
+
+		if (retTemporalC == 0) { // Eliminamos el archivo
 			log_info(logger, "El archivo fue eliminado satisfactoriamente\n");
 		} else {
 			log_info(logger, "No se pudo eliminar el archivo\n");
@@ -1528,6 +1544,15 @@ void comandoCreate(char* tabla, char* consistencia, char* particiones,
 				rutaParticion(i);
 				FILE* particion;
 				particion = fopen(archivoParticion, "w");
+				char* lineaParticion = malloc(string_length("SIZE=")+ sizeof(int)+ string_length("BLOCK=[]")+ sizeof(int)+4);
+				lineaParticion = string_new();
+				string_append(&lineaParticion,"SIZE=0");
+				//string_append(&lineaParticion,"1");
+				string_append(&lineaParticion,"\n");
+				string_append(&lineaParticion,"BLOCK=[");
+				string_append(&lineaParticion,"1");
+				string_append(&lineaParticion,"]");
+				fputs(lineaParticion,particion);
 				log_info(logger, "Particion creada: %s", archivoParticion);
 				fclose(particion);
 			}
@@ -1602,7 +1627,6 @@ void comandoInsert(char* tabla, char* key, char* value, char* timestamp) {
 				mutexDesbloquear(&listaTablasInsertadas_mx);
 
 			}
-
 
 			/*free(valueDesenmascarado);
 			 free(registroPorAgregarE);
