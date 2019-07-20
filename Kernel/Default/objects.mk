2721a2b75be33d784b9db327b5f4d2661c6f56d6
martin <mzuccotti@est.frba.utn.edu.ar>
2019-07-20 17:38:25 -0300

    corrijo makefiles kernel

--- Kernel/Default/objects.mk
+++ Kernel/Default/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /home/utnso/tp-2019-1c-mi_ultimo_segundo_tp/Biblioteca/src/Biblioteca.c /home/untso/tp-2019-1c-mi_ultimo_segundo_tp/Biblioteca/src/Gossiping.c
+USER_OBJS := /home/utnso/tp-2019-1c-mi_ultimo_segundo_tp/Biblioteca/src/Biblioteca.c /home/utnso/tp-2019-1c-mi_ultimo_segundo_tp/Biblioteca/src/Gossiping.c
 
 LIBS := -lreadline -lcommons -lpthread -lm
 
