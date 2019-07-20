69100472e60ce90a650f2ee5329f9dd67feee33b
gianlvalentini <gian.valentini1997@gmail.com>
2019-07-20 16:01:48 -0300

    makefiles

---
+++ LissandraFileSystem/Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS := /home/utnso/tp-2019-1c-mi_ultimo_segundo_tp/Biblioteca/src/Biblioteca.c
+
+LIBS := -lreadline -lcommons -lm -lpthread
+
