1d70daef5645579513557d3e05b18c1c760eb052
Martin <martin_zuc@hotmail.com>
2019-07-20 15:51:25 -0300

    makefile LFS

---
+++ LissandraFileSystem/Default/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../InotifyLFS.c \
+../LissandraFileSystem.c \
+../lfsComunicacion.c \
+../parser.c 
+
+OBJS += \
+./InotifyLFS.o \
+./LissandraFileSystem.o \
+./lfsComunicacion.o \
+./parser.o 
+
+C_DEPS += \
+./InotifyLFS.d \
+./LissandraFileSystem.d \
+./lfsComunicacion.d \
+./parser.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O2 -g -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
